6e56d1cd0415b3140355e52287c7a155e426ee5d
vl4dio4n <vladioanbirsan1@gmail.com>
2022-12-28 15:35:20 +0200

    Small fix

--- src/rw_lock_writer_priority.c
+++ src/rw_lock_writer_priority.c
@@ -12,11 +12,11 @@ void RW_Lock_Writer_Priority_init(RW_Lock_Writer_Priority* rw_lock){
 }
 
 void RW_Lock_Writer_Priority_wait(RW_Lock_Writer_Priority* rw_lock, int mode){
-    if(mode == 1){
+    if(mode == 1){ // Writer Mode
         Semaphore_wait(rw_lock->no_readers);
         Lightswitch_lock(rw_lock->read_switch, rw_lock->no_writers);
         Semaphore_signal(rw_lock->no_readers);
-    } else if(mode == 0){
+    } else if(mode == 0){ // Reader Mode  
         Lightswitch_lock(rw_lock->write_switch, rw_lock->readers);
         Semaphore_wait(rw_lock->no_writers);
     } else {
@@ -26,9 +26,9 @@ void RW_Lock_Writer_Priority_wait(RW_Lock_Writer_Priority* rw_lock, int mode){
 }
 
 void RW_Lock_Writer_Priority_signal(RW_Lock_Writer_Priority* rw_lock, int mode){
-    if(mode == 1){
+    if(mode == 1){ // Writer Mode
         Lightswitch_unlock(rw_lock->read_switch, rw_lock->no_writers)
-    } else if(mode == 0){
+    } else if(mode == 0){ // Reader Mode
         Semaphore_signal(rw_lock->no_writers);
         Lightswitch_unlock(rw_lock->write_switch, rw_lock->no_readers);
     } else {
@@ -42,4 +42,5 @@ void RW_Lock_Writer_Priority_destroy(RW_Lock_Writer_Priority* rw_lock){
     Lightswitch_destroy(rw_lock->write_switch);
     Semaphore_destroy(rw_lock->no_readers);
     Semaphore_destroy(rw_lock->no_writers);
+    free(rw_lock);
 }
